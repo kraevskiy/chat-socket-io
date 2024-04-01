d0ca50cbc10f362db0c7b98c752b7ff3a6e81532
rambutan <pride.kraievskyi@gmail.com>
2024-04-01 19:05:20 +0300

    fix

--- back-end/controllers/auth.controller.ts
+++ back-end/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from "express";
+import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import { TUser, TUserLogin } from "../types";
 import { errorTexts, successTexts } from "../texts";
